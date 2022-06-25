c51b26afef78c6f8ced6d3bf3053d807c0289de3
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-06-25 22:17:29 +0900

    fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,27 +37,18 @@ const MyDocument = () => {
         />
         <meta name="description" content={content} />
         <meta property="og:title" content={content} />
-        <meta
-          property="og:description"
-          content="2020年から2021年の活動をまとめました"
-        />
+        <meta property="og:description" content={content} />
         <meta name="keywords" content="プログラミング, Web制作" />
         <meta property="og:type" content="blog" />
         <meta property="og:url" content={url} />
         <meta property="og:image" content={image} />
         <meta property="og:site_name" content={content} />
-        <meta
-          name="twitter:card"
-          content="2020年から2021年の活動をまとめました"
-        />
-        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta name="twitter:card" content={content} />
+        <meta name="twitter:card" content="summary"></meta>
         <meta name="twitter:site" content="@yokoiwasaki6" />
         <meta name="twitter:url" content={image} />
         <meta name="twitter:title" content={content} />
-        <meta
-          name="twitter:description"
-          content="2020年から2021年の活動をまとめました"
-        />
+        <meta name="twitter:description" content={content} />
         <meta name="twitter:image" content={image} />
         <link rel="canonical" href={url} />
         <link rel="shortcut icon" href={"/favicon.png"} />
