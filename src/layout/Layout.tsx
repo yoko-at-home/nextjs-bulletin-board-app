056493b62a843ddacc11249de0724294744eeb55
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-06-29 00:38:33 +0900

    installed classcat

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,14 +1,24 @@
+import cc from "classcat";
 import { FC, ReactNode } from "react";
 import { Footer } from "./Footer";
 import { HeadPart } from "../layout/HeadPart";
 
 type Props = {
   children: ReactNode;
+  bgColor: "white" | "gray";
 };
 
 export const Layout: FC<Props> = (props) => {
   return (
-    <div className="bg-gray-200 grid grid-rows-[1fr, auto]">
+    <div
+      className={cc([
+        "grid grid-rows-[1fr, auto]",
+        {
+          "bg-gray-200": props.bgColor === "gray",
+          "bg-white": props.bgColor === "white",
+        },
+      ])}
+    >
       <HeadPart />
       <div className="min-h-[90vh]">{props.children}</div>
       <Footer />
