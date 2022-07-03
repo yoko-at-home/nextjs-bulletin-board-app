090d19ca8d408b0bf1695815ceb3e3b05a1f4103
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-07-03 16:51:32 +0900

    hid previous code

--- src/pages/api/send.js
+++ src/pages/api/send.js
@@ -1,20 +1,20 @@
-const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: "moats_array0a@icloud.com", // Change to your recipient
-  from: "yoko_iwasakijp@yahoo.co.jp", // Change to your verified sender
-  subject: "Sending with SendGrid is Fun",
-  text: "and easy to do anywhere, even with Node.js",
-  html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-};
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log("Email sent");
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+// const sgMail = require("@sendgrid/mail");
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// const msg = {
+//   to: "moats_array0a@icloud.com", // Change to your recipient
+//   from: "yoko_iwasakijp@yahoo.co.jp", // Change to your verified sender
+//   subject: "Sending with SendGrid is Fun",
+//   text: "and easy to do anywhere, even with Node.js",
+//   html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+// };
+// sgMail
+//   .send(msg)
+//   .then(() => {
+//     console.log("Email sent");
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
 
 const handler = (req, res) => {
   let msg = {};
